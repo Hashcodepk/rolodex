e9139df18b214bf1f387364e9774ee7e4fa51b4d
hashcodepk <hashirlives@gmail.com>
2020-09-18 19:38:57 +0500

    Updates

---
+++ precache-manifest.7feaa06bae31662fd0412826ff8bea60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9eacec267afce76f5a7c8ca4a7d8620",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "d36eb563cae2b4501579",
+    "url": "/rolodex/static/css/main.389f5607.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36eb563cae2b4501579",
+    "url": "/rolodex/static/js/main.d9ae2dcd.chunk.js"
+  },
+  {
+    "revision": "ce484491ce0eb150302f",
+    "url": "/rolodex/static/js/runtime-main.867464ce.js"
+  }
+]);
